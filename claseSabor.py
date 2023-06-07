f8b183a80806fa7c8705deee3206a63509ce05b7
SantiagoFigueroa04 <132835488+SantiagoFigueroa04@users.noreply.github.com>
2023-06-07 19:23:54 -0300

    Delete claseSabor.py

--- claseSabor.py
+++
@@ -1,9 +0,0 @@
-class Sabor:
-    __idSabor = 'int'
-    __nombreSabor = 'string'
-    __ingredientes = 'string'
-
-    def __init__(self, idSabor, nombreSabor, ingredientes):
-        self.__idSabor = idSabor
-        self.__nombreSabor = nombreSabor
-        self.__ingredientes = ingredientes
